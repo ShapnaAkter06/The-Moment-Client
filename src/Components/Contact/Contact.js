c23be4b32a5121b37e9ac76b851b8f7788d742a2
Shapna <shapna088@gmail.com>
2022-11-08 04:37:38 +0600

    about contact added

---
+++ src/Components/Contact/Contact.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Contact = () => {
+    return (
+        <div className='bg-blue-900 text-white grid grid-cols-1 lg:grid-cols-2 gap-5 p-12 mb-12'>
+            <h2 className='text-6xl '>Thank you for putting your trust in me </h2>
+            <div>
+                <h2 className='text-4xl mb-5'>Let's Work Together</h2>
+                <p className='w-4/5'>Thank you for being so generous with your time, and for your careful research regarding our personal preferences prior to my photography session with you.</p>
+            </div>
+        </div>
+    );
+};
+
+export default Contact;
--- src/Components/Home/About/About.js
+++ src/Components/Home/About/About.js
@@ -6,10 +6,9 @@ const About = () => {
         <div className='grid grid-cols-1 md:grid-cols-2 gap-5 my-12 p-12 items-center bg-gray-200'>
             <img src={about} alt="" />
             <div>
-                <h2 className='text-4xl font-medium mb-5 uppercase'>Who we are</h2>
-                <p>The Moments was created by a group of young engineering students of Islamic University of Technology (IUT), lead by ZK Nawaf who chose their passion as their profession and this incident of their life have been pushing them constantly to expand their potentiality. <br /> <br /> 
-                Right now The Moments has the highest number of fan following among Bangladeshi wedding photography Facebook pages. Among many other achievements we recognize this as the most important one as it directly indicates how many people are relating with us personally. Their likes and comments inspire us to do better every day. <br /><br />
-                In the last 6 years we have covered more than 6800+ events. Our team is now a strong team of 68 Photographers, cinematographers & Editors. All our photographers are highly educated, most of them are engineers. We have two dedicated branch in Dhaka & Chittagong to meet the demand of clients all over bangladesh. Recently we have taken Dream Weaver to an international level by launching our oce in Singapore , Malaysia and Greece specially for the client who want their pre/post wedding abroad. We have started Dream Weaver Youth Academy (DWYA) on 2015.
+                <h2 className='text-4xl font-medium mb-5 uppercase'>About Me</h2>
+                <p>
+                    Photography has always been one of my favorite hobbies. When I take pictures, it's like seeing things in a new perspective literally and figuratively. you can go up to a flower and see the texture of the petal or put a caterpillar on your finger and see the little hairlike things on it's body. Thing about the camera and the lens is that it lets you see people, places, etc. in a whole new perspective and it helps to see things from a different view. From photography, I realized that getting a different view of things means you have a more open mind to the world. Which is why itss always been my favorite hobby.
                 </p>
             </div>
         </div>
