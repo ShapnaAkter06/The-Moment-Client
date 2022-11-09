0d2772b95378de1054d2ec2162b634b571c5452b
Shapna <shapna088@gmail.com>
2022-11-10 04:38:15 +0600

    remove comment

--- src/api/auth.js
+++ src/api/auth.js
@@ -17,7 +17,6 @@ export const setAuthToken = (user) => {
         .then(res => res.json())
         .then(data => {
             console.log(data);
-            //local storage is easiest but not the best place to store jwt token
             localStorage.setItem('the-moment', data.token);
         })
 }
