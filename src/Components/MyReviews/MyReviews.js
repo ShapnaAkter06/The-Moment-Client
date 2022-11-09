c8e838a0da5f076d458f7dd80ebf9168eb40cf3e
Shapna <shapna088@gmail.com>
2022-11-09 15:15:34 +0600

    delete option added

--- src/Components/MyReviews/MyReviews.js
+++ src/Components/MyReviews/MyReviews.js
@@ -12,12 +12,32 @@ const MyReviews = () => {
             .then(data => setMyReviews(data))
     }, [])
 
+    // for delete
+    const handleDelete = id => {
+        const proceed = window.confirm("Are you sure want to cancel this order?");
+        if (proceed) {
+            fetch(`http://localhost:5000/myReviews/${id}`, {
+                method: 'DELETE',
+            })
+                .then(res => res.json())
+                .then(data => {
+                    // console.log(data);
+                    if (data.deletedCount > 0) {
+                        alert('Deleted Successfully');
+                        const remaining = myReviews.filter(myRev => myRev._id !== id);
+                        setMyReviews(remaining)
+                    }
+                })
+        }
+    }
+
     return (
         <div className="overflow-x-auto w-full my-12">
                 <h2 className='text-2xl font-bold text-center mb-5'>My Review</h2>
-                <table className="table mx-auto w-1/2 text-center">
+                <table className="table mx-auto w-4/5 text-center mb-12">
                     <thead>
                         <tr>
+                            <th></th>
                             <th>Name</th>
                             <th>Email ID</th>
                             <th>Service name</th>
@@ -29,6 +49,7 @@ const MyReviews = () => {
                             myReviews.map(myReview => <MyReviewsCard
                                 key={myReview._id}
                                 myReview={myReview}
+                                handleDelete={handleDelete}
                             ></MyReviewsCard>)
                         }
                     </tbody>
--- src/Components/MyReviews/MyReviewsCard.js
+++ src/Components/MyReviews/MyReviewsCard.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
-const MyReviewsCard = ({myReview}) => {
-    const { customer, email, message, name } = myReview;
+const MyReviewsCard = ({myReview, handleDelete}) => {
+    const { customer, email, message, name, _id } = myReview;
     return (
         <tr>
+            <th>
+                <label>
+                    <button onClick={()=> handleDelete(_id)} className="btn btn-ghost">
+                        X
+                    </button>
+                </label>
+            </th>
             <td>
                 {customer}
             </td>
