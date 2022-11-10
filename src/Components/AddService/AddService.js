29f58699376ea1979b61b663ed957ce45134ad4f
Shapna <shapna088@gmail.com>
2022-11-10 22:00:51 +0600

    added bg

--- src/Components/AddService/AddService.js
+++ src/Components/AddService/AddService.js
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import toast from 'react-hot-toast';
 import { AuthContext } from '../../Contexts/AuthProvider/AuthProvider';
 import useTitle from '../../Hooks/useTitle';
+import service from '../../assets/service.jpg'
 
 const AddService = () => {
     useTitle('Add Service');
@@ -36,82 +37,86 @@ const AddService = () => {
 
     };
     return (
-        <div className='m-12'>
-            <form onSubmit={handleServiceSubmit}>
-                <div className="mb-1 sm:mb-2">
-                    <h2 className='mb-5 text-center text-2xl font-bold'>Add Service</h2>
-                    <label
-                        htmlFor="ServiceName"
-                        className="inline-block mb-1 font-medium"
-                    >
-                        Service Name
-                    </label>
-                    <input
-                        placeholder="Service Name"
-                        required
-                        type="text"
-                        className="flex-grow w-full h-12 px-4 mb-2 transition duration-200 bg-white border border-gray-300 rounded shadow-sm appearance-none focus:border-deep-purple-accent-400 focus:outline-none focus:shadow-outline"
-                        id="ServiceName"
-                        name="ServiceName"
-                    />
-                </div>
-                <div className="mb-1 sm:mb-2">
-                    <label
-                        htmlFor="details"
-                        className="inline-block mb-1 font-medium"
-                    >
-                        Service Details
-                    </label>
-                    <input
-                        placeholder="Service Details"
-                        required
-                        type="text"
-                        className="flex-grow w-full h-12 px-4 mb-2 transition duration-200 bg-white border border-gray-300 rounded shadow-sm appearance-none focus:border-deep-purple-accent-400 focus:outline-none focus:shadow-outline"
-                        id="details"
-                        name="details"
-                    />
-                </div>
-                <div className="mb-1 sm:mb-2">
-                    <label
-                        htmlFor="price"
-                        className="inline-block mb-1 font-medium"
-                    >
-                        Service Price
-                    </label>
-                    <input
-                        placeholder="Price"
-                        required
-                        type="text"
-                        className="flex-grow w-full h-12 px-4 mb-2 transition duration-200 bg-white border border-gray-300 rounded shadow-sm appearance-none focus:border-deep-purple-accent-400 focus:outline-none focus:shadow-outline"
-                        id="price"
-                        name="price"
-                    />
-                </div>
-                <div className="mb-1 sm:mb-2">
-                    <label
-                        htmlFor="photo"
-                        className="inline-block mb-1 font-medium"
-                    >
-                        Service Photo
-                    </label>
-                    <input
-                        placeholder="Photo URL"
-                        required
-                        type="text"
-                        className="flex-grow w-full h-12 px-4 mb-2 transition duration-200 bg-white border border-gray-300 rounded shadow-sm appearance-none focus:border-deep-purple-accent-400 focus:outline-none focus:shadow-outline"
-                        id="photo"
-                        name="photo"
-                    />
-                </div>
-                <div className="mt-4 mb-2 sm:mb-4">
-                    <button
-                        type="submit"
-                        className="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-black transition duration-200 rounded shadow-md btn-primary focus:outline-none"
-                    >
-                        Submit
-                    </button>
-                </div>
-            </form>
+        <div className='lg:h-screen bg-center bg-no-repeat bg-cover flex items-center justify-center' style={{
+            backgroundImage: `url(${service})`
+        }}>
+            <div className='bg-slate-100 w-2/4 mx-auto px-4 my-4'>
+                <form onSubmit={handleServiceSubmit}>
+                    <div className="mb-1 sm:mb-2">
+                        <h2 className='mb-5 text-center text-2xl font-bold pt-2'>Add Service</h2>
+                        <label
+                            htmlFor="ServiceName"
+                            className="inline-block mb-1 font-medium"
+                        >
+                            Service Name
+                        </label>
+                        <input
+                            placeholder="Service Name"
+                            required
+                            type="text"
+                            className="flex-grow w-full h-12 px-4 mb-2 transition duration-200 bg-white border border-gray-300 rounded shadow-sm appearance-none focus:border-deep-purple-accent-400 focus:outline-none focus:shadow-outline"
+                            id="ServiceName"
+                            name="ServiceName"
+                        />
+                    </div>
+                    <div className="mb-1 sm:mb-2">
+                        <label
+                            htmlFor="details"
+                            className="inline-block mb-1 font-medium"
+                        >
+                            Service Details
+                        </label>
+                        <input
+                            placeholder="Service Details"
+                            required
+                            type="text"
+                            className="flex-grow w-full h-12 px-4 mb-2 transition duration-200 bg-white border border-gray-300 rounded shadow-sm appearance-none focus:border-deep-purple-accent-400 focus:outline-none focus:shadow-outline"
+                            id="details"
+                            name="details"
+                        />
+                    </div>
+                    <div className="mb-1 sm:mb-2">
+                        <label
+                            htmlFor="price"
+                            className="inline-block mb-1 font-medium"
+                        >
+                            Service Price
+                        </label>
+                        <input
+                            placeholder="Price"
+                            required
+                            type="text"
+                            className="flex-grow w-full h-12 px-4 mb-2 transition duration-200 bg-white border border-gray-300 rounded shadow-sm appearance-none focus:border-deep-purple-accent-400 focus:outline-none focus:shadow-outline"
+                            id="price"
+                            name="price"
+                        />
+                    </div>
+                    <div className="mb-1 sm:mb-2">
+                        <label
+                            htmlFor="photo"
+                            className="inline-block mb-1 font-medium"
+                        >
+                            Service Photo
+                        </label>
+                        <input
+                            placeholder="Photo URL"
+                            required
+                            type="text"
+                            className="flex-grow w-full h-12 px-4 mb-2 transition duration-200 bg-white border border-gray-300 rounded shadow-sm appearance-none focus:border-deep-purple-accent-400 focus:outline-none focus:shadow-outline"
+                            id="photo"
+                            name="photo"
+                        />
+                    </div>
+                    <div className="mt-4 mb-2 sm:mb-4">
+                        <button
+                            type="submit"
+                            className="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-black transition duration-200 rounded shadow-md btn-primary focus:outline-none"
+                        >
+                            Submit
+                        </button>
+                    </div>
+                </form>
+            </div>
         </div>
     );
 };
