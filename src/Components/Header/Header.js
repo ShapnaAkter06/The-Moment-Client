c0d941d01a2bf9e99da6b919f7aa8779e3f5fb02
shapna28 <shapna088@gmail.com>
2023-11-27 16:38:36 +0600

    update the project

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -45,7 +45,7 @@ const Header = () => {
                         {menuItems}
                     </ul>
                 </div>
-                <Link className="btn btn-ghost normal-case text-xl">The Moments</Link>
+                <Link to='/' className="btn btn-ghost normal-case text-xl">The Moments</Link>
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal p-0">
--- src/Components/Home/Banner/Banner.js
+++ src/Components/Home/Banner/Banner.js
@@ -7,7 +7,7 @@ const Banner = () => {
             backgroundImage: `url(${image1})` 
           }}>
             <div className='text-center text-white p-16'>
-                <h2 className='text-4xl mb-5 font-semibold'>Ready to take you beyond your imagination</h2>
+                <h2 className='lg:text-4xl text-2xl mb-5 font-semibold'>Ready to take you beyond your imagination</h2>
                 <p>With the uniqueness of my concepts and hard working quality, I am always a step ahead from others. <br />
                     I take photos so perfectly that you will feel the same that you had felt in that very special day <br /> with your very special one.
                 </p>
